a9743043a9f7f880f86751cf4e8e4e7d1922fab1
Mathias002 <moussetmathias@gmail.com>
2025-09-23 02:37:08 +0200

    Suppression du validator IsOptional pour la description d'une collection

--- backend/src/collections/dto/create-collection.dto.ts
+++ backend/src/collections/dto/create-collection.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCollectionDto {
   @IsNotEmpty()
@@ -8,7 +8,6 @@ export class CreateCollectionDto {
 
   @IsString()
   @MaxLength(500)
-  @IsOptional()
   description: string;
 
   @IsString()
