0db6922a905c09ef3ff2c2daf797a20864763ae9
Mathias002 <moussetmathias@gmail.com>
2025-09-23 02:09:42 +0200

    Ajout d'un fichier admin.model.ts pour mon interface admin + correction des imports + modifications mineures

---
+++ src/app/features/admin/models/admin.model.ts
@@ -0,0 +1,10 @@
+export interface UserInfos {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+  deletedAt: string;
+  createdAt: string;
+  updatedAt: string;
+}
